3676b60f7924afa06b52ab811a4e72c9c9a8ebae
Chris Magnuson <hourglasssand@gmail.com>
2017-03-21 12:39:13 -0400

    Add Test-ConveyorScaleSameWeight functions

--- TervisMonitoring.psm1
+++ TervisMonitoring.psm1
@@ -12,6 +12,12 @@ function Install-TervisMonitoring {
         -Credential $ScheduledTasksCredential `
         -ScheduledTaskFunctionName "Send-TervisDNSServerReport" `
         -RepetitionInterval EveryDayEvery15Minutes
+
+    Install-PowerShellApplicationScheduledTask -PathToScriptForScheduledTask $PathToScriptForScheduledTask `
+        -Credential $ScheduledTasksCredential `
+        -ScheduledTaskFunctionName "Test-ConveyorScaleSameWeight" `
+        -RepetitionInterval EveryMinuteOfEveryDay
+
 }
 
 function Test-TervisDNSServerHealth {
@@ -60,4 +66,48 @@ External Request Test: $($DNSTest.ExternalRequestTest)
 "@
         }
     }
+}
+
+function Test-ConveyorScaleSameWeight {
+    $NumberOfBoxesToSample = 30
+    $ConveyorScaleNumberOfUniqueWeights = Get-ConveyorScaleNumberOfUniqueWeights -NumberOfBoxesToSample $NumberOfBoxesToSample
+
+    if ($ConveyorScaleNumberOfUniqueWeights -eq 1) {
+        Send-ConveyorScaleSameWeightMessage -NumberOfBoxesToSample $NumberOfBoxesToSample
+    }
+
+    if (-not $ConveyorScaleNumberOfUniqueWeights) {
+        Throw "Something went wrong"
+    }
+}
+
+function Get-ConveyorScaleNumberOfUniqueWeights {
+    param (
+        [Parameter(Mandatory)]$NumberOfBoxesToSample
+    )
+    $Query = @"
+SELECT top $NumberOfBoxesToSample
+    ts,
+    weight
+FROM "qc"."ScaleLog"
+order by ts DESC 
+"@
+
+    $Results = Invoke-SQLODBC -DataSourceName tervis -SQLCommand $Query | 
+    ConvertFrom-DataRow
+
+    $ConveyorScaleNumberOfUniqueWeights = $Results | 
+    Group-Object -Property Weight | 
+    measure | 
+    select -ExpandProperty count
+
+    $ConveyorScaleNumberOfUniqueWeights
+}
+
+function Send-ConveyorScaleSameWeightMessage {
+    param (
+        $NumberOfBoxesToSample
+    )
+    $To = "WCSIssues@tervis.com"
+    Send-TervisMailMessage -To $To -Bcc $Bcc -From ITNotification@tervis.com -Subject "ERROR: Last $NumberOfBoxesToSample boxes on the conveyor scale received the same weight" -Body "Last $NumberOfBoxesToSample boxes on the conveyor scale received the same weight.`r`n`r`nPlease check the scale."
 }
